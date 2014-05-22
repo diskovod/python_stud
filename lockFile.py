23054f0203e1c7795dba4e3f7acd3bd3ec5b85df
Diskovod <root@localhost.localdomain>
2014-05-22 18:10:22 +0300

    Lock module

---
+++ lockFile.py
@@ -0,0 +1,24 @@
+
+import warnings
+import fcntl
+
+class FileLock(object):
+	def __init__(self):
+		pass
+
+
+	def lock_file(self, file):
+		#print file
+		#file = open("/media/sf_CentOS/python_stud/myscript.pid", "w")
+		print file.fileno()		
+		try:
+    			fcntl.flock(file.fileno(), fcntl.LOCK_EX|fcntl.LOCK_NB)
+			print "Locked!"
+		except IOError:
+    			warnings.warn("can't immediately write-lock the file ($!), blocking ...")
+    			fcntl.flock(file.fileno(), fcntl.LOCK_EX)
+
+
+#l_cl = FileLock()
+#l_cl.lock_file()
+
