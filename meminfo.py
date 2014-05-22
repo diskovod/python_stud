06b8a7f943e2b7508bc5f0fd455323751406530a
Diskovod <root@localhost.localdomain>
2014-05-22 11:21:17 +0300

    memInfo updated

--- meminfo.py
+++ meminfo.py
@@ -64,23 +64,58 @@ def getLoadAvarage():
 	pattern = "^\((\d+.\d+),\s*(\d+.\d+),\s*(\d+.\d+)"
 	prog = re.compile(pattern)
 	result = prog.match(str_av)
-	
 	fin_res = {'min':result.group(1), 'f_min':result.group(2), 'ft_min':result.group(3)}
 	
 	return fin_res
 
+def getMaxLoadAvarageMin(min):
+	
+	temp = 0.0
+	if min > temp:
+		temp = min
+
+	return temp
+
+def getMaxLoadAvarageFmin(f_min):
+
+        temp = 0.0
+        if f_min > temp:
+                temp = f_min
+
+        return temp
+	
+def getMaxLoadAvarageFtmin(ft_min):
+
+        
+        if ft_min > temp:
+                temp = ft_min
+		max = temp
+	
+        return temp
+
+
 
 	
 memInfo = get_meminfo()
+
 usedMemPerc = getUsedMemoryPercent(memInfo)
 usedMem = getUsedMemory(memInfo)
 usedSwapPerc = getUsedSwapPercent(memInfo)
 usedSwap = getUsedSwap(memInfo)
+
 loadAv = getLoadAvarage()
+min = float(loadAv["min"])
+f_min = float(loadAv["f_min"])
+ft_min = float(loadAv["ft_min"])
 
-print loadAv
+max_min = getMaxLoadAvarageMin(min)
+max_fmin = getMaxLoadAvarageMin(f_min)
+max_ftmin = getMaxLoadAvarageMin(ft_min)
 
-#print """ Used Memory: %s/%s%%  \n Used Swap: %s/%s%% \n Load Avarage: %s""" % (usedMem, usedMemPerc, usedSwap, usedSwapPerc, load_av)
+
+print """==========Load Avarage============= \n For 1 minute: %.2f; \n For 5 minutes: %.2f; \n For 15 minutes: %.2f;  """ % (min, f_min, ft_min)
+
+print """============Used Memory Stat======= \n Used Memory: %s/%s%%  \n Used Swap: %s/%s%% \n""" % (usedMem, usedMemPerc, usedSwap, usedSwapPerc)
 
 
 
