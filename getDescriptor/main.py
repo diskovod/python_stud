1a3ff95c9052af95872c37f137a8552c58f1cc2d
Diskovod <root@localhost.localdomain>
2014-05-27 15:44:56 +0300

    getDescriptor 27.05

--- getDescriptor/main.py
+++ getDescriptor/main.py
@@ -11,41 +11,56 @@ class getDescriptor(object):
 	def __init__(self):
 		pass
 
+	
+        def getType(self, path):
+                if(os.path.isfile(path) == True):
+                        return "file"
+                elif (os.path.isdir(path) == True):
+                        return "dir"
+                elif (os.path.islink(path) == True):
+                        return "link"
+                return "unkn"
+
+
 	def getPidsData(self):
 		pr = os.listdir('/proc/')
 		pattern = "(\d+)"
 		pids = re.findall(pattern, str(pr))
-		
+
 		return pids	
 
+        def getFileType(self, fd):
+                        type = self.getType(fd)
+                        if type == 'file':
+                                return fd
+			else:
+				return ""
 	def getFiles(self, pids):
 			
-		for pid in pids:
-			#pid = read_pid.readPid()
-			open_files = []
-			fd_dir=os.path.join('/proc', str(pid), 'fd')
-			fds = os.listdir(fd_dir)
-			
-			
-		for file in fds:
+		open_files = {}
 
-			link=os.readlink(os.path.join(fd_dir, file))
-			open_files.append(link)
-
-		#for link in sorted(open_files.keys()):
-
-		#	print "%s : %s" % (link, ", ".join(map(str, open_files[link])))
-		return fd_dir
-
-	def getType(self, path):
-        	if(os.path.isfile(path) == True):
-            		return "file"
-        	elif (os.path.isdir(path) == True):
-            		return "dir"
-        	elif (os.path.islink(path) == True):
-            		return "link"
-        	return "unkn"
-
+		for pid in sorted(pids):
+				
+			fd_dir=os.path.join('/proc', pid, 'fd')
+                	fds = os.listdir(fd_dir)
+	
+			for file in fds:
+				try:
+					link=os.readlink(os.path.join(fd_dir, file))
+					files_only = self.getFileType(link)
+					
+						#print type(files_only)
+					if files_only != "":	
+						#int(pid)
+						open_files.setdefault(link, []).append(pid)	
+						open_files[pid] = open_files
+						#open_files.update({pid: files_only})
+						
+				except OSError:
+					continue
+					
+			print open_files
+				
 	def getPath(self, files):
 	
 		print files	
@@ -59,14 +74,7 @@ class getDescriptor(object):
 			i += 1
 		print fin		
 
-	def getListType(self, files):
-		fin = [] 
-		for f in files:
-			type = self.getType(f)
-			if type == 'file':
-				fin.append(f)
-		print fin	
-		
+				
 
 cl = getDescriptor()
 #f = cl.getFiles()
