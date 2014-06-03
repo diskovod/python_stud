bff77cf179755c9ec4dec05745de1eaf3fc4a799
root <root@localhost.localdomain>
2014-06-04 00:05:24 +0300

    Changing catchTerm

--- catchTerm/forking.py
+++ catchTerm/forking.py
@@ -17,7 +17,7 @@ class Child(object):
         print "Is alive :" + str(self.pid) + " = " + str(self.alive)
         return self.alive
 
-    def Terminate(self):
+    def Terminate(self, signal, frame):
         print "Termination: %d" % (self.pid)
         self.terminate = True
 
@@ -26,28 +26,37 @@ class Child(object):
 
     def Run(self):
         self.alive = True
-        while not self.terminate:
-            print "I'm child: %d: id %i " % (self.pid, id(self))
-            time.sleep(2)
-        self.alive = False
+        #print self.terminate
+        print "I'm child: %d: id %i " % (self.pid, id(self))
+        time.sleep(5)
+        #elf.alive = False
+
 
 class Parent(object):
     def __init__(self):
         self.childlist = []
+        self.termProc = False
 
     def TerminateAll(self):
-        for child in self.childlist:
-            print "Terminate child with id: %i" % (id(child))
-            if child.IsAlive():
-                child.Terminate()
-                os.waitpid(child.pid, 0)
+        print self.childlist
+        if self.termProc == True:
+            for child in self.childlist:
+                isWork = os.kill(child, 0)
+                if isWork == None:
+                    print "Terminate child with id: %i" % (child)
+                    #os.waitpid(child, 0)
+                    os.kill(child, signal.SIGTERM)
+                    time.sleep(5)
+                    self.childlist.pop(0)
             else:
                pass
-    
+        print self.childlist           
+
     def signal_term_handler(self, signal, frame):
         
+        self.termProc = True
         print 'got SIGTERM'
-
+        self.TerminateAll()
         print self.childlist
             
     def CreateChild(self):
@@ -55,20 +64,24 @@ class Parent(object):
         cur_pid = os.getpid()
 
         pid = os.fork()
-
+        #pid = 1
         if pid > 0:
             print "Child created: %s : id %i" % (pid, id(child))
             self.childlist.append(pid)
-        else:    
+        else:
+            print "Par pid %d" % (cur_pid)
             print "In child id %i" % (id(child))
             child.setPid(os.getpid())
             child.Run()
 
 pr = Parent()
 
+print "1: CreateChild"
 pr.CreateChild()
+print "2: CreateChild"
 pr.CreateChild()
+print "3: CreateChild"
 
-time.sleep(10)
-pr.TerminateAll()
-#$ignal.signal(signal.SIGTERM, pr.signal_term_handler)
+signal.signal(signal.SIGTERM, pr.signal_term_handler)
+time.sleep(50)
+#pr.TerminateAll()
