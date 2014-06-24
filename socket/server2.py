07f945daf23c1e97af075b4ed8f39fd9ddf11444
Diskovod <root@localhost.localdomain>
2014-06-24 18:32:22 +0300

    sockets updated

--- socket/server2.py
+++ socket/server2.py
@@ -4,6 +4,8 @@ import select
 import socket
 import sys
 import time
+import logging
+
 
 class Server(object):
     def __init__(self, server):
@@ -12,11 +14,20 @@ class Server(object):
         self.backlog = 5 
         self.size = 1024
         self.server = server
-        self.client_list = [server]
+        self.server_list = [server]
         self.client_dict = {}
+        self.client_data = {}
 
     def bindServer(self):
-        return self.server.bind((self.host,self.port))
+
+        try:
+
+            return self.server.bind((self.host,self.port))
+
+        except socket.error, e:
+
+            print "Error is %s" % (e)
+            sys.exit()
 
     def listenServer(self):
         self.server.listen(self.backlog)
@@ -26,64 +37,79 @@ class Server(object):
         for s in inputready:
 
             client, address = self.server.accept()
-           # print "%s connected" % (address)
-            print address
-            self.client_list.append(client)
             
+            print "Client connected: %s" % (str(address))
 
-    def client_read(self, inputready):
+            self.client_logging(1, str(address))
+            self.client_dict.update({client: address})
+            self.client_data[client] = '' 
+            print self.client_data
+             
 
-        for s in inputready:
-
+    def client_read(self, readready):
+        
+        for s in readready:
+            #socket = self.client_dict
             data = s.recv(self.size)
             if data:
                 print data
-                print client_list
+                self.client_data[s] = data
             else:
-                s.close()
-                self.client_list.remove(s)
 
+                s.close()
+                print "Client disconnected: %s" % (str(self.client_dict[s]))
+                self.client_logging(0,str(self.client_dict[s]))
+                del self.client_dict[s]
+                del self.client_data[s]
+                
+
+    def client_write(self, writeready):
+       
+
+        for s in writeready:
+
+            if self.client_data[s]:
+     
+                s.send(self.client_data[s])
+                self.client_data[s] = ''
+
+    def client_logging(self, status, address):
+
+        logger = logging.getLogger('myapp')
+        hdlr = logging.FileHandler('/var/log/pythoninfo.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.setLevel(logging.INFO)
+
+        if status == 0:
+            logger.info("Client disconnected: %s" % (address))
+        elif status == 1:
+            logger.info("Client connected: %s" % (address))
+        else:
+            logger.info("Unknown error!!!")
+
+                
     def main(self):
 
-        #client_list = [self.server]
-        running = 1
-        while running:
+        try:
+            while True:
 
-            inputready,outputready,exceptready = select.select(self.client_list,self.client_list,self.client_list, 0)
+                inputready,outputready,exceptready = select.select(self.server_list,self.server_list,self.server_list, 0)
+                self.client_connect(inputready)
 
-        #    for s in inputready:
+                readready, writeready, exceptread = select.select(self.client_dict,self.client_dict,self.client_dict, 0)
+                self.client_read(readready)
 
-        #        if s == self.server:
-                    # handle the server socket
-
-
-            self.client_connect(inputready)
-            self.client_read(inputready)
-            time.sleep(0.1)
+                readready, writeready, exceptread = select.select(self.client_dict,self.client_dict,self.client_dict, 0)
+                self.client_write(writeready)
+                time.sleep(0.1)
         
-        self.server.close()
+            self.server.close()
+        except KeyboardInterrupt:
+            print "Programm closed"
 
 
-            
-
-            #    elif s == sys.stdin:
-                    # handle standard input 
-            #        junk = sys.stdin.readline()
-                   # running = 0 
-
-           # else:
-           #         # handle all other sockets 
-           #         data = s.recv(self.size)
-           #         if data:
-           # #     s.send(data)
-           #             print data
-           #         else:
-           #             print input
-           #             s.close()
-           #             input.remove(s)
-           #            # running = 0
-           #            # break
-       
 try:
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -98,4 +124,6 @@ s = Server(server)
 s.bindServer()
 s.listenServer()
 s.main()
+
+
 
